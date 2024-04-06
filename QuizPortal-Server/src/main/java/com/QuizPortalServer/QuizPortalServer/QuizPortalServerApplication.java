e0b60e23f46e16d241463068ee49e14077d1ef61
Shubham kumar jha <Shubham Kumar Jha>
2024-04-06 13:28:54 +0530

    First commit

---
+++ QuizPortal-Server/src/main/java/com/QuizPortalServer/QuizPortalServer/QuizPortalServerApplication.java
@@ -0,0 +1,13 @@
+package com.QuizPortalServer.QuizPortalServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizPortalServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizPortalServerApplication.class, args);
+	}
+
+}
---
+++ QuizPortal-Server/src/test/java/com/QuizPortalServer/QuizPortalServer/QuizPortalServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.QuizPortalServer.QuizPortalServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizPortalServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
